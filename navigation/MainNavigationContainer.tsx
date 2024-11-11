3310fc8c55dc38b6530a60e94908e7972028f9f2
BrunoGaxiola <A01253874tec.mx>
2024-11-10 18:44:02 -0700

    feat: fully functional navbar

--- navigation/MainNavigationContainer.tsx
+++ navigation/MainNavigationContainer.tsx
@@ -2,6 +2,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import TransaccionesPage from "../screens/TransaccionesPage";
 import TerminalesPage from "../screens/TerminalesPage";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Fontisto from '@expo/vector-icons/Fontisto';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -15,11 +18,34 @@ function MainNavigationContainer() {
         backgroundColor: '#000000',
         height: 96,
         position: 'absolute',
+        paddingTop: 10
+      },
+      tabBarActiveTintColor: '#0E9600',
+      tabBarInactiveTintColor: '#FFFFFF',
+      tabBarLabelStyle: {
+        fontFamily: 'Montserrat',
+        fontSize: 15
       }
     }}
     >
-      <Tab.Screen name='Transacciones'component={TransaccionesPage} />
-      <Tab.Screen name='Terminales' component={TerminalesPage} />
+      <Tab.Screen 
+      name='Transacciones'
+      component={TransaccionesPage}
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <FontAwesome name="money" color={focused ? '#0E9600' : '#FFFFFF'} size={30} />
+        ),
+      }}
+      />
+      <Tab.Screen 
+      name='Terminales' 
+      component={TerminalesPage} 
+      options={{
+        tabBarIcon: ({ focused }) => (
+          <Fontisto name="shopping-pos-machine" color={focused ? '#0E9600' : '#FFFFFF'} size={30} />
+        ),
+      }}
+      />
     </Tab.Navigator>
   );
 }
